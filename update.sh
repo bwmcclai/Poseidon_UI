f281ffeb82eab8c05acf723b32df3fd77c4b4d5b
bwmcclai <bwmcclai@gmail.com>
2017-01-21 15:47:04 -0500

    Fixed a typo
    flow_raspberrypi.json to flows_raspberrypi.json

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 #sudo apt-get install nginx -y
 #bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
 sudo ln -s /home/pi/Poseidon_UI/html /var/www/html
-ln /home/pi/Poseidon_UI/flow_raspberrypi.json /home/pi/.node-red/flow_raspberrypi.json
+ln /home/pi/Poseidon_UI/flows_raspberrypi.json /home/pi/.node-red/flows_raspberrypi.json
 cd /home/pi/.node-red
 sudo /etc/init.d/nginx start
 node-red-stop
