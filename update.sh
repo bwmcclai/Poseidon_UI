3ca54c1941822f62746957e5ed1a2b484394aca1
bwmcclai <bwmcclai@gmail.com>
2017-01-21 15:45:27 -0500

    Added Update Script
    Use this to update the html and nodered flows only.  Does not install
    nginx and node-red

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#sudo apt-get install nginx -y
+#bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
+sudo ln -s /home/pi/Poseidon_UI/html /var/www/html
+ln /home/pi/Poseidon_UI/flow_raspberrypi.json /home/pi/.node-red/flow_raspberrypi.json
+cd /home/pi/.node-red
+sudo /etc/init.d/nginx start
+node-red-stop
+node-red-start
